8fd477ac02294682a0584ee46bdedbd10373968d
ojma21 <ojma05@gmail.com>
2026-04-01 18:37:40 -0300

    fix deploy

--- server.js
+++ server.js
@@ -170,6 +170,8 @@ app.get("/api/match-stats/:id", async (req, res) => {
 
 
 // 🚀 SERVER EN RED
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
 });
